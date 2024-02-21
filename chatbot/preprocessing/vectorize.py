795ee693c92aeed58cd1ae24b1c668d98062c406
clay.tyler <ctyler9@me.com>
2024-02-21 16:37:08 -0600

    added vectorize/test from ragatouille

--- chatbot/preprocessing/vectorize.py
+++ chatbot/preprocessing/vectorize.py
@@ -8,16 +8,21 @@ env_vars = dotenv_values(".env")
 
 RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
 
-def get_forum_post():
-    headers = []
+def get_files():
+    header = []
     out = []
-    anon_data_root = env_vars["anondata_root"]
+    anon_data_root = env_vars["class_files"]
     for file in os.listdir(anon_data_root):
-        if file.endswith(".json"):
-            data = json.load(os.path.join(anon_data_root, file))
+        with open(os.path.join(anon_data_root, file)) as fp:
+            file_contents = fp.read()
+            
+            header.append(file)
+            out.append(file_contents)
+    
+    return out
 
-def get_syllabus():
-    syllabus_path = env_vars.get("syllabus_path")
+def get_file():
+    syllabus_path = env_vars.get("class_files")
     if not syllabus_path:
         return
 
@@ -27,5 +32,5 @@ def get_syllabus():
     return [file_contents]
 
     
-index_path = RAG.index(index_name="syllabus", collection=get_syllabus())
+index_path = RAG.index(index_name="syllabus", collection=get_files())
 
