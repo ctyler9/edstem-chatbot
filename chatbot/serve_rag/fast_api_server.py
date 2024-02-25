31c98659a162a2d74c4c2da90a0bc17ccf7b7ed6
Clay Tyler <ctyler9@me.com>
2024-02-25 12:41:40 -0600

    experimented with adding fastapi server to fix issue

--- chatbot/serve_rag/fast_api_server.py
+++ chatbot/serve_rag/fast_api_server.py
@@ -1,12 +1,10 @@
-from flask import Flask, request
+from fastapi import FastAPI, Request
 from functools import lru_cache
-import os
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
-app = Flask(__name__)
+app = FastAPI()
 counter = {"api" : 0}
 
 # RAG IMPORT 
@@ -42,20 +40,13 @@ rag = RAG()
 @lru_cache(maxsize=1000000)
 def api_search_query(query):
     pred = rag(query)
-
     return {"query": query, "answer": pred.answer, "context": pred.context}
 
 
-@app.route("/api/search", methods=["GET"])
-def api_search():
-    if request.method == "GET":
-        counter["api"] += 1
-        print("API request count:", counter["api"])
-        return api_search_query(request.args.get("query"))
-    else:
-        return ('', 405)
-
-if __name__ == "__main__":
-    app.run("0.0.0.0", port=int(os.getenv("PORT")))
-
+@app.get("/api/search")
+async def api_search(query: str):
+    global counter
+    counter["api"] += 1
+    print("API request count:", counter["api"])
+    return api_search_query(query)
 
